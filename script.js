16810b71569016bf0b4d62ffa11dedabfbc4241b
Burdet Leila <leilaburdet@gmail.com>
2022-03-15 10:41:20 +0100

    Implement start and pause functions

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 console.log("Ready !");
 window.addEventListener("load", () => {
 	console.log("Hello world !");
+
+	let timer = document.querySelector("#timer");
+	let ms = 0;
+	let startBtn = document.querySelector("#start");
+	let pauseBtn = document.querySelector("#pause");
+	let stopBtn = document.querySelector("#stop");
+	let id;
+
+	startBtn.addEventListener("click", start);
+	pauseBtn.addEventListener("click", pause);
+
+	function start() {
+		id = setInterval(ticking, 10);
+		function ticking(){
+			timer.innerHTML = ms++;
+		}
+	};
+
+	function pause() {
+		clearInterval(id);
+	};
 });
